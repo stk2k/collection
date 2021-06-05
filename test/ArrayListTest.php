b66af39869b4fb7544e59e74b50e929d034f146d
Katsuki Shuto <stk2k@sazysoft.com>
2021-06-06 03:59:16 +0900

    Signed-off-by: Katsuki Shuto <stk2k@sazysoft.com>

--- test/ArrayListTest.php
+++ test/ArrayListTest.php
@@ -218,6 +218,7 @@ class ArrayListTest extends TestCase
 
         $list = new ArrayList([12, 3, -1, 0, 4, 1.2, 'kiwi', 'apple']);
         $ret = $list->sort();
+        print_r($list->toArray());
         $this->assertSame([-1, 0, 'apple', 'kiwi', 1.2, 3, 4, 12], $list->toArray());
         $this->assertSame([-1, 0, 'apple', 'kiwi', 1.2, 3, 4, 12], $ret->toArray());
         $this->assertInstanceOf(ArrayList::class, $ret);
